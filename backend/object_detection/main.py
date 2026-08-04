ce6cd56f430011ce413c4b83e4c3b3f16539970d
Barath <barathjack77@gmail.com>
2026-08-04 20:03:22 +0530

    chore: rename PORT environment variable to BACKEND_PORT in uvicorn configuration

--- backend/object_detection/main.py
+++ backend/object_detection/main.py
@@ -105,6 +105,6 @@ async def websocket_stream(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8889))
+    port = int(os.environ.get("BACKEND_PORT", 8889))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
